9b58fd15087a6a76373a6e613a2e8d3e9dd89de3
mekhan <margub.khan@gmail.com>
2022-02-02 12:55:32 +0530

    PPMTool on gradle build

--- PPMTool/src/main/java/com/agilemargub/ppmtool/domain/Project.java
+++ PPMTool/src/main/java/com/agilemargub/ppmtool/domain/Project.java
@@ -1,5 +1,8 @@
 package com.agilemargub.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -14,18 +18,26 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank
+    @NotBlank(message = "Project name is blank")
     private String projectName;
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier; // projectId may be ambiguous for JPA
+    @NotBlank(message = "Project description is required")
     private String description;
 
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
-    public Project(){
+    public Project() {
 
     }
 
